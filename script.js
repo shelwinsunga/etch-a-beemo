af2090237acd49e06e78226e83107d4760c1e27e
shelwinsunga <shelwinsunga@gmail.com>
2022-07-30 20:25:28 -0700

    dynamic resolution resizing buttons functional + clear button redo

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const container = document.getElementById("grid-container");
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("cell");
-
+var size = 16;
 
 
 //Creates a default grid sized 16x16 
@@ -19,8 +19,6 @@ function makeRows(rowNum) {
     for (r = 0; r < rowNum; r++) {
         let row = document.createElement("div");
         container.appendChild(row).className = "gridRow";
-
-        
     };
 };
 //Creates columns
@@ -30,7 +28,9 @@ function makeColumns(cellNum) {
             //calculate enough pixels to change resolution without changing total size.
             var validRes = 800 / cellNum;
             let newCell = document.createElement("div");
+
             rows[j].appendChild(newCell).className = "cell";
+            newCell.id = i + "," + j;
             newCell.style.minHeight= validRes + "px";
             newCell.style.minWidth= validRes + "px";
         };
@@ -38,17 +38,52 @@ function makeColumns(cellNum) {
     };
 };
 
-defaultGrid(16);
+function destroyGrid(){
+    container.innerHTML='';
+}   
+
+
+
+function changeResolution(){
+
+        const btn16 = document.getElementById('16px');
+        const btn32 = document.getElementById('32px');
+        const btn64 = document.getElementById('64px');
+
+        btn16.addEventListener('click', () => { 
+            destroyGrid();
+            size = 16;
+            defaultGrid(size);
+            draw();
+        });
+
+        btn32.addEventListener('click', () => { 
+            destroyGrid();
+            size = 32;
+            defaultGrid(size);
+            draw();
+        });
+
+        btn64.addEventListener('click', () => { 
+            destroyGrid();
+            size = 64;
+            defaultGrid(size);
+            draw();
+        });
+    
+    
+}
+
+
+defaultGrid(size);
+
 
 const allCells = document.querySelectorAll(".cell");
 
 var mouseClicking = false;
 
-
-
-
-
 function draw(){
+    const allCells = document.querySelectorAll(".cell");
     var pixelColor = document.getElementById("color-picker");
     var aColor = "#002222";
     
@@ -70,6 +105,11 @@ function draw(){
             }
         });
 
+        cell.addEventListener("click", () =>{
+            cell.style.backgroundColor=aColor;
+
+        });
+
         pixelColor.addEventListener("input", () =>{
             aColor = pixelColor.value;
         });
@@ -81,21 +121,43 @@ function draw(){
 
 
 function clearGrid(){
+    
+    const allCells = document.querySelectorAll(".cell");
     const clearbtn = document.getElementById('clear');
     clearbtn.addEventListener('click', () => { 
-        allCells.forEach((cell) =>{
-            cell.style.backgroundColor="#c5e5d6";
-
-        });
-
-
+        destroyGrid();
+        defaultGrid(size);
+        draw();
     });
 
 }
 
+
+
+
+// function saveState(){
+//      var state =  [];
+//      var cellInfo = [];
+
+
+
+//     const savebtn = document.getElementById('save');
+//     savebtn.addEventListener('click', () => { 
+//         allCells.forEach((cell) =>{
+//             cellInfo = [cell.id,window.getComputedStyle(cell).backgroundColor];
+//             state.push(cellInfo);
+//          });
+    
+//          console.log(state);
+
+//     });
+// }
+
 function main(){
+    changeResolution();
     draw();
     clearGrid();
+    // saveState();
 }
 
 main();
